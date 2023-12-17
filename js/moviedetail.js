8f5dcea031a9c3c42c9877d5ff8c5ea9459c8679
giimguang <giimguangmao@gmail.com>
2023-12-17 19:02:32 +0700

    update 12/17/2023 7:02

---
+++ js/moviedetail.js
@@ -0,0 +1,13 @@
+var trailer = document.getElementById("trailer");
+function openTrailer(){
+    trailer.classList.add("open-trailer");
+}
+function closeTrailer(){
+    trailer.classList.remove("open-trailer");
+}
+
+
+var more = document.getElementById("more-popup");
+function openMorePopup(){
+   more.classList.toggle("open-more-popup");
+}
